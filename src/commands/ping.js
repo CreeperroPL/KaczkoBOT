2e457406e3ca0bf3f61a85f1b0fb3e2409d27532
CreeperroPL <115413586+CreeperroPL@users.noreply.github.com>
2022-11-01 13:06:59 +0100

    dsa
    das

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -2,7 +2,7 @@ module.exports = {
     name: ["ping"],
     async execute(client, msg, args) {
 
-            msg.channel.send(`Pong! Mam ${client.ws.ping} ms pingu `)
+            msg.channel.send(`Pong! \nMam **${client.ws.ping}** ms pingu `)
 
             console.log(`Użytkownik ${msg.author.tag} użył komende: !${args}`)
     }
